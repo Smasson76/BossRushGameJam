83011ccf668addd7be339ff19623c843a6cd515e
norkskargrot <56662825+norkskargrot@users.noreply.github.com>
2021-06-30 17:33:10 +1000

    added first version of player controller and a demo scene

---
+++ Assets/Scripts/Player/Control.cs
@@ -0,0 +1,263 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Control.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @Control : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @Control()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Control"",
+    ""maps"": [
+        {
+            ""name"": ""PlayerMovement"",
+            ""id"": ""f9c6c382-2e93-465a-bdab-759ceb4fab1c"",
+            ""actions"": [
+                {
+                    ""name"": ""Camera Control"",
+                    ""type"": ""Value"",
+                    ""id"": ""581a2498-6894-4dcb-aaac-90f2aa745b1d"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""PlayerMove"",
+                    ""type"": ""Value"",
+                    ""id"": ""a9f4318c-01ed-4fc8-9cfd-bf218ac41d69"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""GrappleStart"",
+                    ""type"": ""Button"",
+                    ""id"": ""104df7e0-9e95-4ea3-ac2b-fbcac7f7b222"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": ""Press""
+                },
+                {
+                    ""name"": ""GrappleEnd"",
+                    ""type"": ""Button"",
+                    ""id"": ""8946be8e-be71-424a-9bfe-d013c9b9efea"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": ""Press(behavior=1)""
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""1eeaa0b0-2cba-4db4-a9a4-410adcc8d73d"",
+                    ""path"": ""<Mouse>/position"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Camera Control"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""2D Vector"",
+                    ""id"": ""cbaedf55-d1be-4d5c-90c4-ad3a08cc576e"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""PlayerMove"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""3eac39f5-4fd5-42e9-ae43-3e488b01b97f"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""PlayerMove"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""e34f73a8-0ce0-417e-aaa2-d6d684bf5118"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""PlayerMove"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""27ff8398-af0e-41bc-9c22-54de56962c37"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""PlayerMove"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""704478cf-5c90-406b-a6b5-54d0a8c46638"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""PlayerMove"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""0365e732-fc94-40dc-8fb4-43a7a77b4788"",
+                    ""path"": ""<Mouse>/leftButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""GrappleStart"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""4b1ee1cf-5f32-435d-95ba-10ee57661312"",
+                    ""path"": ""<Mouse>/leftButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""GrappleEnd"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // PlayerMovement
+        m_PlayerMovement = asset.FindActionMap("PlayerMovement", throwIfNotFound: true);
+        m_PlayerMovement_CameraControl = m_PlayerMovement.FindAction("Camera Control", throwIfNotFound: true);
+        m_PlayerMovement_PlayerMove = m_PlayerMovement.FindAction("PlayerMove", throwIfNotFound: true);
+        m_PlayerMovement_GrappleStart = m_PlayerMovement.FindAction("GrappleStart", throwIfNotFound: true);
+        m_PlayerMovement_GrappleEnd = m_PlayerMovement.FindAction("GrappleEnd", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // PlayerMovement
+    private readonly InputActionMap m_PlayerMovement;
+    private IPlayerMovementActions m_PlayerMovementActionsCallbackInterface;
+    private readonly InputAction m_PlayerMovement_CameraControl;
+    private readonly InputAction m_PlayerMovement_PlayerMove;
+    private readonly InputAction m_PlayerMovement_GrappleStart;
+    private readonly InputAction m_PlayerMovement_GrappleEnd;
+    public struct PlayerMovementActions
+    {
+        private @Control m_Wrapper;
+        public PlayerMovementActions(@Control wrapper) { m_Wrapper = wrapper; }
+        public InputAction @CameraControl => m_Wrapper.m_PlayerMovement_CameraControl;
+        public InputAction @PlayerMove => m_Wrapper.m_PlayerMovement_PlayerMove;
+        public InputAction @GrappleStart => m_Wrapper.m_PlayerMovement_GrappleStart;
+        public InputAction @GrappleEnd => m_Wrapper.m_PlayerMovement_GrappleEnd;
+        public InputActionMap Get() { return m_Wrapper.m_PlayerMovement; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerMovementActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerMovementActions instance)
+        {
+            if (m_Wrapper.m_PlayerMovementActionsCallbackInterface != null)
+            {
+                @CameraControl.started -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnCameraControl;
+                @CameraControl.performed -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnCameraControl;
+                @CameraControl.canceled -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnCameraControl;
+                @PlayerMove.started -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnPlayerMove;
+                @PlayerMove.performed -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnPlayerMove;
+                @PlayerMove.canceled -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnPlayerMove;
+                @GrappleStart.started -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnGrappleStart;
+                @GrappleStart.performed -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnGrappleStart;
+                @GrappleStart.canceled -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnGrappleStart;
+                @GrappleEnd.started -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnGrappleEnd;
+                @GrappleEnd.performed -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnGrappleEnd;
+                @GrappleEnd.canceled -= m_Wrapper.m_PlayerMovementActionsCallbackInterface.OnGrappleEnd;
+            }
+            m_Wrapper.m_PlayerMovementActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @CameraControl.started += instance.OnCameraControl;
+                @CameraControl.performed += instance.OnCameraControl;
+                @CameraControl.canceled += instance.OnCameraControl;
+                @PlayerMove.started += instance.OnPlayerMove;
+                @PlayerMove.performed += instance.OnPlayerMove;
+                @PlayerMove.canceled += instance.OnPlayerMove;
+                @GrappleStart.started += instance.OnGrappleStart;
+                @GrappleStart.performed += instance.OnGrappleStart;
+                @GrappleStart.canceled += instance.OnGrappleStart;
+                @GrappleEnd.started += instance.OnGrappleEnd;
+                @GrappleEnd.performed += instance.OnGrappleEnd;
+                @GrappleEnd.canceled += instance.OnGrappleEnd;
+            }
+        }
+    }
+    public PlayerMovementActions @PlayerMovement => new PlayerMovementActions(this);
+    public interface IPlayerMovementActions
+    {
+        void OnCameraControl(InputAction.CallbackContext context);
+        void OnPlayerMove(InputAction.CallbackContext context);
+        void OnGrappleStart(InputAction.CallbackContext context);
+        void OnGrappleEnd(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInput : MonoBehaviour {
+
+     [Header("References")]
+    [SerializeField] private GameObject player;
+    [SerializeField] private Transform playerCamera;
+
+     [Header("Camera")]
+    [SerializeField] private float lookTargetHeight;
+    [SerializeField] private float cameraDistance;
+    [SerializeField] private float cameraMinTilt;
+    [SerializeField] private float cameraMaxTilt;
+    [SerializeField] private float cameraMaxSpeed;
+    [SerializeField] private float cameraDeadZone;
+    [SerializeField] private float cameraGradientSize;
+
+    [Header("Movement")]
+    [SerializeField] private float acceleration;
+
+    private Control controls;
+    private Vector2 cameraPos = Vector2.zero;
+    private Rigidbody playerRb;
+    private Transform lookTarget;
+    private Vector3 grapplePoint;
+    private SpringJoint grappleSpring; 
+    private LineRenderer rope;
+    private bool isGrappling = false;
+
+    void Awake() {
+        controls = new Control();
+        controls.PlayerMovement.GrappleStart.performed += ctx => GrappleStart(ctx);
+        controls.PlayerMovement.GrappleEnd.performed += ctx => GrappleEnd(ctx);
+    }
+    void OnEnable() {
+        controls.Enable();
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+    void OnDisable() {
+        controls.Disable();
+        Cursor.lockState = CursorLockMode.None;
+    }
+    void Start() {
+        playerRb = player.GetComponent<Rigidbody>();
+        GameObject lookTargetGameObject = new GameObject("Look Target");
+        lookTarget = lookTargetGameObject.transform;
+        rope = this.gameObject.GetComponent<LineRenderer>();
+        rope.enabled = false;
+        PositionLookTarget();
+        PositionCamera();
+    }
+    void Update() {
+        PositionLookTarget();
+        CameraMove();
+        if (isGrappling) {
+            UpdateRope();
+        }
+        PlayerMove();
+    }
+    void PositionLookTarget() {
+        lookTarget.position = player.transform.position + Vector3.up * lookTargetHeight;
+    }
+
+    void CameraMove() {
+        Vector2 val = controls.PlayerMovement.CameraControl.ReadValue<Vector2>();
+        // Camera movement horizontally
+        val.x -= Screen.width/2;
+        float maxSpeedLocation = cameraDeadZone + cameraGradientSize;
+        if (val.x > 0) {val.x = Mathf.Clamp(UtilityFunctions.Remap(val.x, cameraDeadZone, maxSpeedLocation, 0, cameraMaxSpeed), 0, cameraMaxSpeed);}
+        if (val.x < 0) {val.x = Mathf.Clamp(UtilityFunctions.Remap(val.x, -maxSpeedLocation, -cameraDeadZone, 0, cameraMaxSpeed), -cameraMaxSpeed, 0);}
+        cameraPos.x += val.x;
+
+        // Camera movement vertically
+        cameraPos.y = UtilityFunctions.Remap(val.y, Screen.height, 0, cameraMinTilt, cameraMaxTilt);
+        cameraPos.y = Mathf.Clamp(cameraPos.y, -cameraMaxTilt, cameraMaxTilt);
+        PositionCamera();
+    }
+
+    void PositionCamera() {
+        Vector3 offsetVector = Quaternion.Euler(cameraPos.y, cameraPos.x, 0) * new Vector3(0, 0, -cameraDistance);
+        playerCamera.position = player.transform.position + offsetVector;
+        playerCamera.LookAt(lookTarget);
+    }
+
+    void PlayerMove() {
+        Vector2 val = controls.PlayerMovement.PlayerMove.ReadValue<Vector2>();
+        Vector3 forceDirection = (Quaternion.Euler(0, cameraPos.x, 0) * new Vector3(val.x, 0, val.y)).normalized;
+        playerRb.AddForce(forceDirection * acceleration, ForceMode.Force);
+    }
+
+    void GrappleStart(InputAction.CallbackContext ctx) {
+        Debug.Log("Grapple started");
+        RaycastHit hit;
+        Vector2 mousePos = controls.PlayerMovement.CameraControl.ReadValue<Vector2>();
+        Ray ray = Camera.main.ScreenPointToRay(mousePos);
+        if (Physics.Raycast(ray, out hit)) {
+            Debug.Log("hit something");
+            isGrappling = true;
+            
+            grapplePoint = hit.point;
+            grappleSpring = player.AddComponent<SpringJoint>();
+            grappleSpring.autoConfigureConnectedAnchor = false;
+            grappleSpring.connectedAnchor = grapplePoint;
+            float grappleDist = Vector3.Distance(player.transform.position, grapplePoint);
+            grappleSpring.maxDistance = grappleDist * 0.8f;
+            grappleSpring.minDistance = grappleDist * 0.3f;
+            grappleSpring.spring = 5;
+            grappleSpring.damper = 5f;
+
+            rope.enabled = true;
+        }   
+    }
+
+    void GrappleEnd(InputAction.CallbackContext ctx) {
+        Debug.Log("Grapple ended");
+        isGrappling = false;
+        Destroy(grappleSpring);
+        rope.enabled = false;
+    }
+
+    void UpdateRope() {
+        rope.positionCount = 2;
+        rope.SetPosition(0, player.transform.position);
+        rope.SetPosition(1, grapplePoint);
+    }
+}
