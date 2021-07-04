c0a3ddb1d6782701717fb2ef0baef0611356ceac
Skylar Masson <smasson1998@gmail.com>
2021-07-03 20:17:40 -0500

    Added volume for each sound

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -12,21 +12,25 @@ public class AudioManager : MonoBehaviour {
     public AudioClip[] grappleAudioClips;
     public int minGrapplePitch = 1;
     public int maxGrapplePitch = 3;
+    public float grappleVolume = 0.3f;
 
     [Header("Reel Return Sound Manager")]
     public AudioClip[] reelReturnAudioClips;
     public int minReelReturnPitch = 1;
     public int maxReelReturnPitch = 3;
+    public float reelReturnVolume = 0.3f;
 
     [Header("Booster Sound Manager")]
-    public AudioClip boosterAudioClips;
+    public AudioClip[] boosterAudioClips;
     public int minBoosterPitch = 1;
     public int maxBoosterPitch = 3;
+    public float boosterVolume = 0.3f;
 
     [Header("Player Movement Sound Manager")]
     public AudioClip[] movementAudioClips;
     public int minMovementPitch = 1;
     public int maxMovementPitch = 3;
+    public float movementVolume = 0.3f;
 
     void Awake() {
         if (instance == null) {
@@ -41,24 +45,28 @@ public class AudioManager : MonoBehaviour {
     public void PlayGrappleSoundEffect() {
         audioSource.clip = grappleAudioClips[Random.Range(0, grappleAudioClips.Length)];
         audioSource.pitch = Random.Range(minGrapplePitch, maxGrapplePitch);
+        audioSource.volume = grappleVolume;
         audioSource.Play();
     }
 
     public void PlayReelReturnSoundEffect() {
         audioSource.clip = reelReturnAudioClips[Random.Range(0, reelReturnAudioClips.Length)];
         audioSource.pitch = Random.Range(minReelReturnPitch, maxReelReturnPitch);
+        audioSource.volume = reelReturnVolume;
         audioSource.Play();
     }
 
     public void PlayBoosterSoundEffect() {
-        //audioSource.clip = boosterAudioClips[Random.Range(0, boosterAudioClips.Length)];
-        //audioSource.pitch = Random.Range(minBoosterPitch, maxBoosterPitch);
-        audioSource.PlayOneShot(boosterAudioClips, 10f);
+        audioSource.clip = boosterAudioClips[Random.Range(0, boosterAudioClips.Length)];
+        audioSource.pitch = Random.Range(minBoosterPitch, maxBoosterPitch);
+        audioSource.volume = boosterVolume;
+        audioSource.Play();
     }
 
     public void PlayerMovementSoundEffect() {
         audioSource.clip = movementAudioClips[Random.Range(0, movementAudioClips.Length)];
         audioSource.pitch = Random.Range(minMovementPitch, maxMovementPitch);
+        audioSource.volume = movementVolume;
         audioSource.Play();
     }
 }
