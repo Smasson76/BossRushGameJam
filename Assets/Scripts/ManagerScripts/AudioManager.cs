bfd627c6a72b2b9b3cbc29fa5ea27be915047a97
Skylar Masson <smasson1998@gmail.com>
2021-07-08 16:15:26 -0500

    Sound starts under start function

--- Assets/Scripts/ManagerScripts/AudioManager.cs
+++ Assets/Scripts/ManagerScripts/AudioManager.cs
@@ -4,7 +4,29 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour {
     
-    public static AudioManager instance;
+    public static AudioManager instance; //Creating a singleton object
+
+    //This allows us to use EventRef attribute and will present the designer with the UI for selecting events
+    [FMODUnity.EventRef]
+    public string PlayerStateEvent = "";
+
+    //EventInstance class will allow us to manage an event over it's lifetime. Including Starting, stopping, and changing parameters
+    FMOD.Studio.EventInstance playerState;
+
+    //These events are one shot sounds. They are sounds that have a finite length. 
+    //We do not store an EventInstance to manage the sounds. Once started they will play to completion.
+    [FMODUnity.EventRef]
+    public string DamageEvent = "";
+    [FMODUnity.EventRef]
+    public string HealEvent = "";
+
+    //One shot event system that will have a tracked state and take action when it ends. Could also change parameter values over the lifetime.
+    [FMODUnity.EventRef]
+    public string PlayerIntroEvent = "";
+    FMOD.Studio.EventInstance playerIntro;
+
+    FMOD.Studio.PARAMETER_ID healthParameterId, fullHealthParameterId;
+
 
     void Awake() {
         if (instance == null) {
@@ -14,5 +36,19 @@ public class AudioManager : MonoBehaviour {
         else {
             Destroy(gameObject);
         }
+    }
+
+    void Start() {
+        
+    }
+
+    public void DeathSound() {
+        playerState = FMODUnity.RuntimeManager.CreateInstance(PlayerStateEvent);
+        playerState.start();
+
+        playerIntro = FMODUnity.RuntimeManager.CreateInstance(PlayerIntroEvent);
+        playerIntro.start();
+
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(playerIntro, GetComponent<Transform>());
     }
 }
