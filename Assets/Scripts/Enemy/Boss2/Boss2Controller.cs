1a70e4b6d58e2cfefb66be5ddf1af114fc8af61d
Skylar Masson <32114503+Smasson76@users.noreply.github.com>
2021-07-29 00:17:56 -0500

    Created homing missile for boss2

--- Assets/Scripts/Enemy/Boss2/Boss2Controller.cs
+++ Assets/Scripts/Enemy/Boss2/Boss2Controller.cs
@@ -7,12 +7,24 @@ public class Boss2Controller : MonoBehaviour {
 
     public static Boss2Controller instance; //Creating a singleton
     
-    [SerializeField] Transform playerTransform;
+    [SerializeField] Transform playerTransform; //Player transform
 
+    //For Nav Mesh
     Vector3 destination;
     Vector3 lastPosition;
 
+    //GameObjects
     NavMeshAgent agent;
+    public Rigidbody missile;
+    public GameObject forceField;
+    public GameObject[] missilePoints;
+
+    //Variables
+    public int count = 500;
+
+    bool createNewPath = true;
+    public bool shotHomingMissile = false;
+
 
     public enum State {
 		Idle,
@@ -34,11 +46,12 @@ public class Boss2Controller : MonoBehaviour {
         anim = GetComponent<Animator>();
         body = GetComponent<Rigidbody>();
         agent = GetComponent<NavMeshAgent>();
+        forceField.SetActive(true);
         destination = agent.destination;
     }
 
     public void InitalizeFindPlayer() {
-        playerTransform = GameObject.FindWithTag("Player").transform;
+        playerTransform = GameObject.Find("Sphere").transform;
         IdleUpdate();
     }
 
@@ -51,6 +64,7 @@ public class Boss2Controller : MonoBehaviour {
                 ChaseUpdate();
                 break;
             case State.Shoot:
+                ShootUpdate();
                 break;
             case State.Death:
                 break;
@@ -69,14 +83,32 @@ public class Boss2Controller : MonoBehaviour {
     }
 
     void ChaseUpdate() {
-        //if (Vector3.Distance(destination, playerTransform.position) > 0.001f) {
-            //destination = playerTransform.position;
-            //agent.destination = destination;
-        //}
+        if (createNewPath) StartCoroutine(ChasePlayer());
 
-       if (playerTransform.position != lastPosition) {
-           agent.SetDestination(playerTransform.position);
-           lastPosition = playerTransform.position;
-       }
+        if (count > 0) count-=1;
+        else state = State.Shoot;
+    }
+
+    IEnumerator ChasePlayer() {
+        agent.SetDestination(playerTransform.position);
+        createNewPath = false;
+        yield return new WaitForSeconds(2f);
+        createNewPath = true;
+    }
+
+    void ShootUpdate() {
+        forceField.SetActive(false);
+
+        if (shotHomingMissile == false) { 
+            StartCoroutine(SpawnHomingMissile());
+        }
+    }
+
+    IEnumerator SpawnHomingMissile() {
+        shotHomingMissile = true;
+        yield return new WaitForSeconds(2f);
+        for (int i = 0; i < missilePoints.Length; i++) {
+            Rigidbody newMissile = Instantiate(missile, missilePoints[i].transform.position, transform.rotation) as Rigidbody;
+        }
     }
 }
---
+++ Assets/Scripts/Enemy/Boss2/HomingMissile.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomingMissile : MonoBehaviour {
+    
+
+    public Transform playerTransform; //Player Transform
+
+    //Public Variables
+    public float homingForce;
+    public float rotationForce;   
+    public float launchForce;
+    public bool shouldFollow = false;
+
+    Rigidbody body;
+
+    void Start() {
+        body = GetComponent<Rigidbody>();
+        playerTransform = GameObject.Find("Sphere").transform;
+        StartCoroutine(WaitBeforeHoming());
+    }
+
+    void FixedUpdate() {
+        if (shouldFollow) {
+            Vector3 dir = playerTransform.position - body.position;
+            dir.Normalize();
+            Vector3 rotationAmount = Vector3.Cross(transform.forward, dir);
+            body.angularVelocity = rotationAmount * rotationForce;
+            body.velocity = transform.forward * homingForce;
+        }
+    }
+
+    IEnumerator WaitBeforeHoming() {
+        body.AddForce(Vector3.up * launchForce, ForceMode.Impulse);
+        yield return new WaitForSeconds(2f);
+        shouldFollow = true;
+    }
+
+    void OnCollisionEnter(Collision c) {
+        Destroy(this.gameObject);
+    }
+}
