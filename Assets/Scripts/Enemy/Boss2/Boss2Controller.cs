473e86fe904f8254b64292111e610b38a7d72ade
Skylar Masson <32114503+Smasson76@users.noreply.github.com>
2021-08-02 17:22:03 -0500

    Boss2 missiles now spawn a random amount

--- Assets/Scripts/Enemy/Boss2/Boss2Controller.cs
+++ Assets/Scripts/Enemy/Boss2/Boss2Controller.cs
@@ -112,9 +112,35 @@ public class Boss2Controller : MonoBehaviour {
     IEnumerator SpawnHomingMissile() {
         shotHomingMissile = true;
         yield return new WaitForSeconds(1.5f);
-        for (int i = 0; i < missilePoints.Length; i++) {
-            Rigidbody newMissile = Instantiate(missile, missilePoints[i].transform.position, transform.rotation) as Rigidbody;
+        
+        switch (Random.Range(1, 4)) {
+            case 1:
+                Rigidbody newMissile1 = Instantiate(missile, missilePoints[0].transform.position, transform.rotation) as Rigidbody;
+                Debug.Log("Case 1 happened");
+                break;
+            case 2:
+                Rigidbody newMissile2 = Instantiate(missile, missilePoints[0].transform.position, transform.rotation) as Rigidbody;
+                Rigidbody newMissile3 = Instantiate(missile, missilePoints[1].transform.position, transform.rotation) as Rigidbody;
+                Debug.Log("Case 2 happened");
+                break;
+            case 3:
+                Rigidbody newMissile4 = Instantiate(missile, missilePoints[0].transform.position, transform.rotation) as Rigidbody;
+                Rigidbody newMissile5 = Instantiate(missile, missilePoints[1].transform.position, transform.rotation) as Rigidbody;
+                Rigidbody newMissile6 = Instantiate(missile, missilePoints[2].transform.position, transform.rotation) as Rigidbody;
+                Debug.Log("Case 3 happened");
+                break;
+            case 4:
+                Rigidbody newMissile7 = Instantiate(missile, missilePoints[0].transform.position, transform.rotation) as Rigidbody;
+                Rigidbody newMissile8 = Instantiate(missile, missilePoints[1].transform.position, transform.rotation) as Rigidbody;
+                Rigidbody newMissile9 = Instantiate(missile, missilePoints[2].transform.position, transform.rotation) as Rigidbody;
+                Rigidbody newMissile10 = Instantiate(missile, missilePoints[3].transform.position, transform.rotation) as Rigidbody;
+                Debug.Log("Case 4 happened");
+                break;
+            default:
+            Debug.Log("Case default happened");
+                break;
         }
+        
         yield return new WaitForSeconds(2f);
         shotHomingMissile = false;
         count = 1200;
